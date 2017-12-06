b556b9a5c86e17b1253586de8625e22903501128
“lencshu <lencshu@gmail.com>
2017-12-06 18:24:20 +0100

    mise en page

--- Clipboard.ahk
+++
@@ -1,8 +0,0 @@
-﻿;将本地图片插入Markdown
-^+c:: ;ctrl+shift+c
-clipboard= ;清空剪贴板
-send, ^c
-clipwait
-clipboard = <p align="center">![](%clipboard%)</p>
-msgbox, 图片路径已复制！%clipboard%
-return
--- Oneforall/Tans_Z.m
+++ Oneforall/Tans_Z.m
@@ -1,5 +1,8 @@
 clc;
 clear;
+
+%{
+
 %Inverse Z-Transform
 syms z;
 F=10*z/((z-1)*(z-2));
@@ -11,7 +14,6 @@ den=[1 -1 2]; %[1 -3 2]; %conv([1 -1/3],[1 -3]);
 [r,p,k]=residue(num,den)
 R=rat(r);
 
-
 %{
 tf
 c2d()
@@ -20,3 +22,26 @@ step
 imp
 zpk
 %}
+
+%}
+
+%
+s=tf('s');
+g=1/(s+1);
+ts=0.1;
+r=1/s;
+%step(R-G*R);
+
+%control function
+c=(500*s+50)/(100*s^2+s);
+cl=feedback(c*g,1);
+%step(1/s-cl/s);
+%bode(c*g);
+
+%zoh采样方法
+cz=c2d(g,ts,'zoh')
+bode(c,cz);
+
+
+
+
